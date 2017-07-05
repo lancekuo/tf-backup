5a047552bf06ab6715debc5952943e2abf662334
Lance Kuo <lancekuo@gmail.com>
2017-07-05 14:07:56 -0400

    Add script path and scheduler into output variables.

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "lambda_backup_create_script" {
+    value = "${aws_lambda_function.ebs-backup-create.filename}"
+}
+output "lambda_backup_delete_script" {
+    value = "${aws_lambda_function.ebs-backup-delete.filename}"
+}
+output "scheduler" {
+    value = "${aws_cloudwatch_event_rule.every_day.schedule_expression}"
+}
